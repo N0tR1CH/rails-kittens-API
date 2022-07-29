f3c53267c46d7b05021d070466626e720a4dd10c
n0tR1CH <damainrichter58@gmail.com>
2022-07-29 12:47:57 +0200

    make kitten serializer test work

--- spec/serializers/kitten_serializer_spec.rb
+++ spec/serializers/kitten_serializer_spec.rb
@@ -3,5 +3,20 @@ require 'devise'
 require 'faker'
 
 describe KittenSerializer do
-  
+  let(:user) { create :user }
+  let!(:auth_headers) { user.create_new_auth_token }
+
+  let(:kitten) { FactoryBot.build(:kitten) }
+  let(:serializer) { described_class.new(kitten, scope: user, scope_name: :current_user) }
+  let(:serialization) { ActiveModelSerializers::Adapter.create(serializer) }
+
+  let(:subject) { JSON.parse(serialization.to_json) }
+
+  it 'has a name that matches' do
+    expect(subject['name']).to eql(kitten.name)
+  end
+
+  it 'has a age that matches' do
+    expect(subject['age']).to eql(kitten.age)
+  end  
 end
