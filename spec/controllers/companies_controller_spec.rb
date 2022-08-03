b2c64fcc634e7ea994784b5dc9a73dd89fee8193
n0tR1CH <damainrichter58@gmail.com>
2022-08-03 11:51:26 +0200

    refactorise house_controller spec a bit

--- spec/controllers/companies_controller_spec.rb
+++ spec/controllers/companies_controller_spec.rb
@@ -2,10 +2,9 @@ require 'rails_helper'
 require 'devise'
 require 'faker'
 
-describe HousesController do
+describe CompaniesController do
   let(:user) { create(:user) }
   let!(:auth_headers) { user.create_new_auth_token }
 
   before { request.headers.merge! auth_headers } 
-      
 end
--- spec/controllers/houses_controller_spec.rb
+++ spec/controllers/houses_controller_spec.rb
@@ -5,17 +5,20 @@ require 'faker'
 describe HousesController do
   let!(:auth_headers) { user.create_new_auth_token }
   let(:user) { create :user }
+
   before { request.headers.merge! auth_headers }
 
   describe "[GET] #index" do
     let(:house1)  { create :house }
     let(:house2)  { create :house }
     let(:house3)  { create :house }
+
     before { get :index }
     
     context 'normal user' do
       context 'with houses' do
         let(:user) { create :user, houses: [house1, house2, house3] }
+
         it { expect(JSON.parse(response.body).count).to eq(3) }
       end
   
@@ -28,6 +31,7 @@ describe HousesController do
     context 'admin' do
       let(:user) { create :admin }
       let!(:houses) { create_list :house, 3}
+
       before { get :index }
       context 'with houses' do
         it { expect(JSON.parse(response.body).count).to eq(3) }
@@ -44,8 +48,10 @@ describe HousesController do
     context 'normal user' do
       let(:house) { create :house}
       let(:house_id) { house.id }
+
       context 'normal user can only see his house' do
         let(:user) { create :user, houses: [house] }
+
         before { get :show, params: {id: house_id} }
         it { expect(JSON.parse(response.body)["id"]).to eq(house_id) } 
       end
@@ -54,6 +60,7 @@ describe HousesController do
         let(:user) { create :user }
         let(:house) { create :house}
         let(:house_id) { house.id }
+
         before { get :show, params: {id: house_id} }
         it { expect(response).to have_http_status(:not_found) }
       end
@@ -64,6 +71,7 @@ describe HousesController do
         let(:user) { create :admin }
         let(:house) { create :house}
         let(:house_id) { house.id }
+
         before { get :show, params: {id: house_id} }
         it { expect(JSON.parse(response.body)["id"]).to eq(house_id) } 
       end
@@ -73,6 +81,7 @@ describe HousesController do
   describe "[POST] #create" do
     let(:house_params) { { house: { street: "street", city: "city" } } }    
     let(:create_request) { post :create, params: house_params }
+
     it { expect { create_request }.to change { House.count }.from(0).to(1) }
   end
 
@@ -91,6 +100,7 @@ describe HousesController do
     context 'user can update his house' do
       let(:house) { create(:house, street: "street", city: "city") }
       let(:user) { create :user, houses: [house]}
+
       it { expect { create_request}.to change { house.reload.street }.from("street").to("new_street") }
       it { expect { create_request}.to change { house.reload.city }.from("city").to("new_city") }
     end
@@ -101,6 +111,7 @@ describe HousesController do
       let(:house_params) { { house: { street: "new_street", city: "new_city" }, id: house_id } }
       let(:house_id) { house.id }
       let(:create_request) { patch :update, params: house_params }
+
       it { expect { create_request }.not_to change { house.reload.street } }
       it { expect { create_request }.not_to change { house.reload.city } }
     end
@@ -112,6 +123,7 @@ describe HousesController do
       let(:user) { create :admin }
       let(:house_id) { house.id }
       let(:create_request) { delete :destroy, params: {id: house_id} }
+
       it { expect { create_request }.to change { House.count }.from(1).to(0) }
     end
 
@@ -120,12 +132,14 @@ describe HousesController do
       let(:user) { create :user, houses: [house] }
       let(:house_id) { house.id }
       let(:create_request) { delete :destroy, params: {id: house_id} }
+
       it { expect { create_request }.to change { House.count }.from(1).to(0) }
     end
       let!(:house) { create :house }
       let(:user) { create :user }
       let(:house_id) { house.id }
       let(:create_request) { delete :destroy, params: {id: house_id} }
+      
       it { expect { create_request }.not_to change { House.count } }
     context "user cannot destroy houses that he does not own" do
 
